c21f86a2cee0ec211c1697c04d969703806dee4b
Shivampeta <sriman.shivampeta@capgemini.com>
2020-12-21 15:42:14 +0530

    added initial configs(space, indents, eslint) and removed unwanted components

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest",
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "vue-jest"
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,vue}", "!src/main.js"]
+};
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+      title: "Dashboard"
+    };
+  }
+};
+</script>
