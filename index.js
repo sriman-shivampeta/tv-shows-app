44060369dd4a1b0f8f5d562f85cbd00707ed1b75
Shivampeta <sriman.shivampeta@capgemini.com>
2020-12-28 15:05:48 +0530

    ghPage branch

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.static("dist"));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"))
+);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log("Running on port " + `${port}`));
